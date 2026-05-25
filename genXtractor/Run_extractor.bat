499e12f2d2fe3a3d8010dbf66785f1f4462b0be4
Micfild7 <32404061+Micfild7@users.noreply.github.com>
2026-05-25 10:41:27 -0300

    Delete genXtractor/Run_extractor.bat

--- genXtractor/Run_extractor.bat
+++
@@ -1,4 +0,0 @@
-cd /d "%~dp0"
-C:\"Program Files"\Python311\python.exe genXtractor.py "C:\Users\Filipe\Desktop\DOUTORADO\PROJECT_S15\Data\newtestset\extended_required.fa" "Output" "C:\Users\Filipe\Desktop\DOUTORADO\PROJECT_S15\Data\Refs\15A_wciZ.fasta" ".10050_2_37.5" 78053 78884 0 0 "-"
-
-pause
