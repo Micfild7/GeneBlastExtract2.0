fe1a69cd913a9669dff9080938763da1c4857b90
Micfild7 <32404061+Micfild7@users.noreply.github.com>
2026-05-25 10:37:26 -0300

    Delete run_complex.bat

--- run_complex.bat
+++
@@ -1,3 +0,0 @@
-cd /d "%~dp0"
-C:\"Program Files"\Python311\python.exe run_complex.py "D:\DR_JUAN_15_PROJECT_ASSEMBLIES\Silico_Serotype_Serogroup_15\ASSEMBLIES" "C:\Users\Filipe\Desktop\DOUTORADO\PROJECT_S15\Data\Refs\15A_wciZ.fasta" "C:\Users\Filipe\Desktop\DOUTORADO\PROJECT_S15\Data\ExtRefs\15A_wciZ_EXT.fasta" "C:\Users\Filipe\Desktop\DOUTORADO\PROJECT_S15\bin" 0 0 
-pause
