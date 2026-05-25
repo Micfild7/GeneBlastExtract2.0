300f7ecd97a7bb203af465b4c54db840c36c3afe
Micfild7 <32404061+Micfild7@users.noreply.github.com>
2026-05-25 10:39:49 -0300

    Delete genBlast/genBLAST_simple_test.bat

--- genBlast/genBLAST_simple_test.bat
+++
@@ -1,4 +0,0 @@
-cd /d "%~dp0"
-C:\"Program Files"\Python311\python.exe genBlast_simple.py "C:\Users\Filipe\Desktop\DOUTORADO\PROJECT_S15\Data\Refs\15A_wciZ.fasta" "C:\Users\Filipe\Desktop\DOUTORADO\PROJECT_S15\Data\newtestset" "C:\Users\Filipe\Desktop\DOUTORADO\PROJECT_S15\bin\genBlast\output"
-
-pause
